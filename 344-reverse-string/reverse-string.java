f31a13c393bd7b5cbbf08bf06358093cecfe6b10
Rishikesh Das <157221030+rishikeshdas887@users.noreply.github.com>
2026-08-29 19:13:42 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (55.47%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void reverseString(char[] s ) {
+       
+       //2 pointer approach amd using 
+       
+
+        int left=0;
+        int right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+
+
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
