f5fcc5d6a50d59c1213de57e153fbbb890193ade
Rishikesh Das <157221030+rishikeshdas887@users.noreply.github.com>
2026-09-08 14:55:09 +0530

    Time: 1 ms (99.92%) | Memory: 45.3 MB (65.36%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int compress(char chars[]) {
+       
+        int write=0; //write will count the length of each character group and in final tells the actual length pof compressed array
+        int read=0;
+
+      while(read<chars.length){
+           char current=chars[read];
+           int count=0;
+
+           while(read<chars.length && chars[read]==current){
+            count++;
+            read++;
+           }
+
+           //tocount how many time one character is repeated consecutively 
+           chars[write++]=current; //wr
+
+
+if(count>1){
+//changing the integer value of the count for a character into string value
+     String countChar=String.valueOf(count);
+
+     for(char c:countChar.toCharArray())
+      chars[write++]=c;
+}
+
+
+
+
+      }
+      return write;
+
+    }
+}
