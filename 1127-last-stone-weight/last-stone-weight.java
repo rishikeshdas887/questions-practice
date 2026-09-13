9ff0026ab07b5926d37b36d7bbe91336e576ef3e
Rishikesh Das <157221030+rishikeshdas887@users.noreply.github.com>
2026-09-13 19:37:32 +0530

    Time: 1 ms (99.66%) | Memory: 43.2 MB (15.36%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lastStoneWeight(int stones[]) {
+        
+PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+//adding elements
+for(int stone:stones){
+    pq.add(stone);
+}
+
+
+while(pq.size()>1){ //going until one step greater than 1 so that maintained
+//picking two heaviest stones
+int y=pq.poll();
+int x=pq.poll();
+
+//checking if they are equal
+    if(x!=y){
+    pq.add(y-x);//heaviest- second heaviest and adding 
+    }
+
+}
+
+return pq.isEmpty() ? 0:pq.poll();
+
+//O(nlog n )because each stone adding will take place so N times and adding one stone takes 0(logN) time
+//space complexity will be O(N) because extra space is needed for stroing all N no.of Stones
+    }
+}
